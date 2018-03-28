76892cf2e9b377841af281f607f9ad315bb60b77
ajung <cwhtha@gmail.com>
2018-03-28 14:12:46 +0900

    0328 flappy bird complete

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -28,7 +28,9 @@ public class Bird : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(){
+		rb2d.velocity = Vector2.zero;
 		isDead = true;
 		anim.SetTrigger ("Die");
+		GameControl.instance.BirdDied ();
 	}
 }
